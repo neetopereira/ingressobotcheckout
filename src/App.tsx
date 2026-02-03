5b2c29cc99d6f8991f58ff618bad60387e0a2021
neetopereira <neto_tva@hotmail.com>
2026-02-03 15:16:04 +0000

    Corrigindo rota creditcard

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
@@ -15,8 +15,13 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          {/* A ROTA MÁGICA QUE FALTAVA 👇 */}
+          <Route path="/creditcard/:orderId" element={<Index />} />
+          
+          {/* Rota padrão para testes (opcional) */}
+          <Route path="/" element={<Navigate to="/404" replace />} />
+          
+          {/* Qualquer outra coisa cai no 404 */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
